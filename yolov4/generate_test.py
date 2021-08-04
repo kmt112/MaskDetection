48957c17a7bc8e1ac44347d253653a2784b6f01f
kmt112 <65839632+kmt112@users.noreply.github.com>
2021-08-05 00:22:05 +0800

    Update generate_test.py
    Split data into train test split.

--- yolov4/generate_test.py
+++ yolov4/generate_test.py
@@ -3,7 +3,7 @@ import os
 image_files = []
 os.chdir(os.path.join("data", "test"))
 for filename in os.listdir(os.getcwd()):
-    if filename.endswith(".jpg"):
+    if filename.endswith(".png"):
         image_files.append("data/test/" + filename)
 os.chdir("..")
 with open("test.txt", "w") as outfile:
